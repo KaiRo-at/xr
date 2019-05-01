312a0655cf117370b578a94f8692a29eaba5c7ef
Robert Kaiser <kairo@kairo.at>
2019-05-02 00:40:58 +0200

    for now, use 0.8 as that still supports collada (we should really move to glTF), add battery and hit count displays to range

--- range/main.js
+++ range/main.js
@@ -2,8 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var hits = 0;
+var hitsDisplay = null;
+
 window.onload = function() {
-  // Hook up trigger button iside the VR.
+  hitsDisplay = document.querySelector("#scoredisplay");
+  // Hook up trigger button inside the VR.
   let leftHand = document.querySelector("#left-hand");
   let rightHand = document.querySelector("#right-hand");
   leftHand.addEventListener("triggerdown", startShooting, false);
@@ -17,27 +21,22 @@ window.onload = function() {
 }
 
 function startShooting(event) {
+  //if (batteryfill <= 0) { return; }
   var rayElement = event.target.querySelector(".ray");
-  rayElement.setAttribute("visible", true);
-  rayElement.setAttribute("raycaster", {enabled: true});
-  // doesn't really work
-  //rayElement.object3D.children[0].material.linewidth = 5;
-  rayElement.components.sound.playSound();
+  rayElement.emit("activate", {el: rayElement}, false);
 }
 
 function stopShooting(event) {
   var rayElement = event.target.querySelector(".ray");
-  rayElement.setAttribute("visible", false);
-  rayElement.setAttribute("raycaster", {enabled: false});
-  // Manually clear any ongoing hits.
-  for (target of rayElement.components.raycaster.intersectedEls) {
-    target.emit("hit-cleared", {el: rayElement}, false);
-  }
-  rayElement.components.sound.stopSound();
+  rayElement.emit("deactivate", {el: rayElement}, false);
 }
 
 function targetHit(event) {
   event.target.emit("hit", {el: event.detail.el}, false);
+  hits++;
+  if (hitsDisplay) {
+    hitsDisplay.setAttribute("text", {value: hits});
+  }
 }
 
 function targetCleared(event) {
